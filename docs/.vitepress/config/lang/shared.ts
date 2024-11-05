a23009efb3bd53fd5b484423ca052b7161d06ef8
isixe <isixe@outlook.com>
2024-11-06 00:58:27 +0800

    🐞 fix(config): fixed favicon icon path

--- docs/.vitepress/config/lang/shared.ts
+++ docs/.vitepress/config/lang/shared.ts
@@ -4,9 +4,13 @@ export const shared = defineConfig({
 	title: "Firefly",
 	lastUpdated: true,
 	srcDir: "pages",
+	vite: {
+		publicDir: "../../../public",
+	},
 	themeConfig: {
 		logo: "/favicon.ico",
 	},
+	head: [["link", { rel: "icon", href: "/favicon.ico" }]],
 	rewrites: {
 		"en/:rest*": ":rest*",
 	},
